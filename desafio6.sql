874e990594aab70046d1155b555344a5427b5ecc
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-10-26 19:35:38 -0300

    Requisito 06 - finalizado.

--- desafio6.sql
+++ desafio6.sql
@@ -1,13 +1,7 @@
-SELECT (
-        SELECT MIN(value)
-        FROM
-            SpotifyClone.subscription_plan
-    ) AS faturamento_minimo, (
-        SELECT MAX(value)
-        FROM
-            SpotifyClone.subscription_plan
-    ) AS faturamento_maximo,
+SELECT
+    CAST( (MIN(s.value)) AS DECIMAL(5, 2)) AS faturamento_minimo,
+    CAST( (MAX(s.value)) AS DECIMAL(5, 2)) AS faturamento_maximo,
     CAST( (AVG(s.value)) AS DECIMAL(5, 2)) AS faturamento_medio,
-    ROUND(SUM(s.value), 2) AS faturamento_total
+    CAST( (SUM(s.value)) AS DECIMAL(5, 2)) AS faturamento_total
 FROM SpotifyClone.users u
     INNER JOIN SpotifyClone.subscription_plan s ON s.subscription_plan_id = u.subscription_plan_id;
