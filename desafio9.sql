664d919d2c438c97ac7bf0527476d30f7cf1b479
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-10-27 15:01:28 -0300

    Requisito 09 - codigo correto.

--- desafio9.sql
+++ desafio9.sql
@@ -1,8 +1,6 @@
 SELECT
-    a.name AS artista,
-    alb.name AS album
-FROM SpotifyClone.artists a
-    INNER JOIN SpotifyClone.albums alb ON a.artists_id = alb.artists_id
-WHERE a.name = "Elis Regina"
-group by a.name, alb.name
-ORDER BY alb.name;
+    COUNT(*) AS quantidade_musicas_no_historico
+FROM SpotifyClone.users u
+    INNER JOIN SpotifyClone.history h ON h.users_id = u.users_id
+WHERE
+    u.name = "Barbara Liskov";
