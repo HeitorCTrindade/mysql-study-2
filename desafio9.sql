e10df693b6c2d7497325570996ccc9bb9e97ce1e
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-10-27 15:00:10 -0300

    Requisito 9 - finalizado.

---
+++ desafio9.sql
@@ -0,0 +1,8 @@
+SELECT
+    a.name AS artista,
+    alb.name AS album
+FROM SpotifyClone.artists a
+    INNER JOIN SpotifyClone.albums alb ON a.artists_id = alb.artists_id
+WHERE a.name = "Elis Regina"
+group by a.name, alb.name
+ORDER BY alb.name;
